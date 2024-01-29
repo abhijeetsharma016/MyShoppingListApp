a55034849a047023bbbe05f465f4a1b0b2034d4e
abhijeetsharma016 <abhijeetsharma16102004@gmail.com>
2024-01-29 19:31:00 +0530

    added location Selection screen

---
+++ app/src/main/java/com/example/myshoppinglistapp/LocationSelectionScreen.kt
@@ -0,0 +1,21 @@
+package com.example.myshoppinglistapp
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.rememberCameraPositionState
+
+@Composable
+fun locationSelectionScreen(
+    location: LocationData,
+    onLocationSelected: (LocationData) -> Unit){
+    var userLocation = remember{
+        mutableStateOf(LatLng(location.latitude, location.longitude))
+    }
+
+    var cameraPositionState = rememberCameraPositionState{
+        position = CameraPosition.fromLatLngZoom(userLocation.value, 10f)
+    }
+}
