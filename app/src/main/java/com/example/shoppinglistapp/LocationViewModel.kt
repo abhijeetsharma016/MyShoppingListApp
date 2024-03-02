5bab502c443dd1ee67a4570ccb6a6845614e4e78
abhijeetsharma016 <abhijeetsharma16102004@gmail.com>
2024-03-02 15:50:59 +0530

    edited locationViewModel

--- app/src/main/java/com/example/shoppinglistapp/LocationViewModel.kt
+++ app/src/main/java/com/example/shoppinglistapp/LocationViewModel.kt
@@ -1,16 +1,35 @@
 package com.example.shoppinglistapp
 
+import android.util.Log
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 
 class LocationViewModel : ViewModel() {
     private  val _location= mutableStateOf<LocationData?>(null)
     val location: MutableState<LocationData?> = _location
 
-
+    private val _address = mutableStateOf(listOf<GeocodingResult>())
+    val address: State<List<GeocodingResult>> = _address
     fun updateLocation(newLocation: LocationData){
         _location.value = newLocation
+    }
+
+
+    fun fetchAddress(latlng: String){
+        try{
+            viewModelScope.launch {
+                val result = RetrofitClient.create().getAddressFromCoordinates(
+                    latlng,
+                    "AIzaSyDLDzPrNJHACEKemEP0Z9EIJ0zE3sp8pTM"
+                )
+                _address.value = result.results
+            }
+        }catch(e:Exception) {
+            Log.d("res1", "${e.cause} ${e.message}")
+        }
     }
 }
