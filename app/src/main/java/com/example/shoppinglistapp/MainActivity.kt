8302c42aebf3e42ee731ca64b8bb6463f9f4f811
abhijeetsharma016 <abhijeetsharma16102004@gmail.com>
2024-03-02 16:19:19 +0530

    added navigation and changed permissions in manifest file

--- app/src/main/java/com/example/shoppinglistapp/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglistapp/MainActivity.kt
@@ -9,7 +9,13 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.dialog
+import androidx.navigation.compose.rememberNavController
 import com.example.shoppinglistapp.ui.theme.ShoppingListAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,8 +28,37 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ShoppingListApp()
+                    Navigation()
                 }
+            }
+        }
+    }
+}
+@Composable
+fun Navigation(){
+    val navController = rememberNavController()
+    val viewModel: LocationViewModel = viewModel()
+    val context = LocalContext.current
+    val locationUtils = LocationUtils(context)
+
+    NavHost(navController, startDestination = "shoppinglistscreen"){
+        composable("shoppinglistscreen") {
+            ShoppingListApp(
+                locationUtils = locationUtils,
+                viewModel = viewModel,
+                navController = navController ,
+                context = context,
+                address = viewModel.address.value.firstOrNull()?.formatted_address ?: "No Address"
+            )
+        }
+
+        dialog("locationscreen"){backstack->
+            viewModel.location.value?.let{it1 ->
+
+                LocationSelectionScreen(location = it1, onLocationSelected = {locationdata->
+                    viewModel.fetchAddress("${locationdata.latitude},${locationdata.longitude}")
+                    navController.popBackStack()
+                })
             }
         }
     }
